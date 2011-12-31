1d96031703753e6fc6b1242e12fe59521378fc2d
bill shelton <bill@if.io>
2011-12-31 11:30:12 -0500

    Added:  - quick panel test.  - run last test

--- mxunit_plugin.py
+++ mxunit_plugin.py
@@ -5,137 +5,174 @@ display the results in s Sublime way.
 """
  
 import os
-import tempfile
 import json
 import datetime
 import re
-import ConfigParser
 from urllib2 import urlopen,HTTPError
 import sublime_plugin
+import sublime
 
 
-#To be specified by developer in a text file ... or can we prompt them here and 
-#then save it to disk?
-# server = 'localhost'
-# port = '8301'
-# component_root = '/'
-# web_root = '/home/billy/software/jrun4/servers/dev/cfusion.ear/cfusion.war/'
-# test_cfc = 'balisong/BalisongTest.cfc'
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class BaseCommand(sublime_plugin.TextCommand):
 
+	def __init__(self, view):
+		global_settings = sublime.load_settings("mxunit.settings")
+		self.last_run_settings = sublime.load_settings("mxunit-last-run.sublime-settings")
+		self.server = global_settings.get('server', 'localhost')
+		self.view = view
+		self.output_view = None
+		#grab the runner config items
+		self.port = global_settings.get('port', '80')
+		self.web_root = global_settings.get('web_root', '/var/www/html/')
+		self._win = None
+		self.test_items = { 'test-1':{'url':'AAAAAA'},
+							'test-2':{'url':'BBBBBB'},
+							'test-3':{'url':'CCCCCC'},
+							}
+	
+	
+	def show_qp(self):
+		panel_items = []
+		self.view.window().show_quick_panel(self.test_items.keys(), self.on_done)	
+	
 
+	def on_done(self,selected_item):
+		keys = self.test_items.keys()
+		key = keys[selected_item]
+		print  self.test_items[ key ]
+	
 
-class mxunit_command(sublime_plugin.TextCommand):
-
-	output_view = None
-	#grab the runner config items
-	config = ConfigParser.ConfigParser()
-	config.read('./mxunit.config')
-	server = config.get('mxunit', 'server')
-	port = config.get('mxunit', 'port')
-	web_root = config.get('mxunit', 'web_root')
-
-	_win = None
-	_edit = None
-	_results = None
-
-	def run(self,edit):
-		
-		view = self.view
-		current_file = view.file_name()
-		#test
-		_test_cfc = current_file.replace(self.web_root, '')
-
-		#temp
-		# _test_cfc = 'balisong/BalisongTest.cfc'
-
-
-		print 'Test: %s' % _test_cfc
-		u = 'http://' + self.server + ':' + self.port + '/' + _test_cfc +'?method=runtestremote&output=json'
-		self._edit = edit
-
-		
+	def run_test(self, url, edit):
 		try:
-			_res = urlopen(u)
-			self._win = view.window()
+			_res = urlopen(url)
+			self._win = self.view.window()
 			self._results = _res.read()
-			view.window().run_command("show_panel", {"panel": "output.tests"})
-			self.output_view = view.window().get_output_panel("tests")
-			self.output_view.insert( edit, self.output_view.size(), self.pretty_results(self._results) ) 
-
+			self.save_test_run(url)
+			
+			self.view.window().run_command("show_panel", {"panel": "output.tests"})
+			self.output_view = self.view.window().get_output_panel("tests")
+			#self.show_tests_panel()
+			self.output_view.insert( edit, self.output_view.size(), pretty_results(self._results) ) 
 		except HTTPError as e:
 			sublime.error_message ('\nRuh roh, Raggy. Are you sure this is a valid MXUnit test?\n\n%s' % e)
-		
-		# self.view.window().show_quick_panel( [0,1,2,3,4], self.onSelect, sublime.MONOSPACE_FONT)
-		
-		# return 1
-		#Good for running adhoc tests
-		#view.window().show_input_panel('Yo. Which test do you want to run? ', '/path/to/test.cfc', None, None, None)
-		#return
-	
-
-
-	def pretty_results(self,test_results):
-		_results = '________________________________________________________\n\n'
-		_results += '    :::::::   MXUnit Test Results  :::::::     \n'
-		_results += '________________________________________________________\n\n'
-		_results += '  Date:  %s\n' % (datetime.datetime.now())
-		tests = json.loads(test_results)
-		passed =  len( [ x for x in tests if x['TESTSTATUS']=='Passed'] )
-		failed =  len( [ x for x in tests if x['TESTSTATUS']=='Failed'] )
-		errors =  len( [ x for x in tests if x['TESTSTATUS']=='Error'] )
-		_results += '  Passed: %s, Failed: %s, Errors: %s\n' % (passed,failed,errors)
-		_results += '________________________________________________________\n'
-
-		#To Do: Calculate totals --total, errors, failures, time
-		#pprint( len(tests) )
-
-		for test in tests:
-			_results += '\n  %s.%s : status=%s\n' % (test['COMPONENT'],test['TESTNAME'], test['TESTSTATUS'] ) 
-			if( test['DEBUG'] ):
-				_debug = test['DEBUG']
-				i=0
-				for var in _debug:
-					# print '%s = %s' % ( var, _debug[i] )
-					_results += " >>Debug: %s\n\n" % (var) 
-				
-			_results += '\n|--------------------------------------------------------------------------------\n'
-		
-		_results += '\n________________________________________________________\n'
-		_results += 'Test results:  Passed=%s, Failed=%s, Errors=%s\n' % (passed,failed,errors)
-		return _results
 
 	
+	#saves it to Packages/User ...?
+	def save_test_run(self, url):
+		print 'Saving url: %s' % url
+		self.last_run_settings.set("last_test_run", url)
+		sublime.save_settings("mxunit-last-run.sublime-settings")
+		print self.last_run_settings.get('last_test_run')
 
 
-	def parse_line(self,line):
-		pattern = re.compile("""
-			(private|package|remote|public)*[ ]*
-			(any|string|array|numeric|boolean|component|struct|void)*[ ]*
-			function[ ]+([a-z][a-z0-9_\-]+)
-			""", 
-			re.VERBOSE|re.MULTILINE|re.IGNORECASE)
-		m = pattern.match(line)
-		#return the 4th gouped regex, which should be the function name
-		return m.group(3)
 
+class ShowQpCommand(BaseCommand):
 
-	
-	def append(self, text, panel_name = 'example'):
-		_view = self.view.window().get_output_panel(panel_name)
-		_view.run_command("show_panel", {"panel": "output." + panel_name})	
-		# return
+	def run(self,edit):
+		self.show_qp()	
 
-		# if not hasattr(self, 'output_view'): 
-		# 	self.output_view = self.window.get_output_panel(panel_name)
-		# 	# _view = self.window.get_output_panel(panel_name)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class MxunitCommand(BaseCommand):
+
+	def run(self,edit):
+		_view = self.view
+		_current_file = _view.file_name()
+		#test
+		_test_cfc = _current_file.replace(self.web_root, '')
+		print 'Test: %s' % _test_cfc
+		_url = 'http://' + self.server + ':' + self.port + '/' + _test_cfc +'?method=runtestremote&output=json'
+		self.run_test(_url, edit)
 		
-		# _view = self.output_view
 
-  #    	# Write this text to the output panel and display it
-  #   	edit = _view.begin_edit()
-  #   	_view.insert(edit, v.size(), text + '\n')
-  #   	_view.end_edit(edit)
-  #   	_view.show(v.size())
-    	# self.view.window().run_command("show_panel", {"panel": "output." + panel_name})	
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class RunLastTestCommand(BaseCommand):
+
+	def run(self,edit):
+		_url = self.last_run_settings.get("last_test_run")
+		self.run_test(_url, edit)
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class SingleTestCommand(BaseCommand):
+
+	def run(self,edit):
+		_view = self.view
+		for region in _view.sel():
+			line = _view.line(region)
+			line_contents = _view.substr(line) + '\n'
+			test_method = parse_line(line_contents)
+		
+		_current_file = _view.file_name()
+		#test
+		_test_cfc = _current_file.replace(self.web_root, '')
+		print 'Test: %s - %s' %  (_test_cfc, test_method,)
+		if test_method == '' : 
+			sublime.error_message ('\nRuh roh, Raggy. The line the cursor is on doesn\'t look like a test.\n\n')
+			return
+		_url = 'http://' + self.server + ':' + self.port + '/' + _test_cfc +'?method=runtestremote&output=json&testmethod=' + test_method
+		self.run_test(_url, edit)
+
+
+
+
+
+
+#########################################################################################
+#                                                                                       #
+#           Utility methods used by all classes (could be in BaseCommand)               #
+#                                                                                       #  
+#########################################################################################
+
+def pretty_results(test_results):
+	_results = '________________________________________________________\n\n'
+	_results += '    :::::::   MXUnit Test Results  :::::::     \n'
+	_results += '________________________________________________________\n\n'
+	_results += '  Date:  %s\n' % (datetime.datetime.now())
+	tests = json.loads(test_results)
+	passed =  len( [ x for x in tests if x['TESTSTATUS']=='Passed'] )
+	failed =  len( [ x for x in tests if x['TESTSTATUS']=='Failed'] )
+	errors =  len( [ x for x in tests if x['TESTSTATUS']=='Error'] )
+	_results += '  Passed: %s, Failed: %s, Errors: %s\n' % (passed,failed,errors)
+	_results += '________________________________________________________\n'
+
+	#To Do: Calculate totals --total, errors, failures, time
+	#pprint( len(tests) )
+
+	for test in tests:
+		_results += '\n  %s.%s : status=%s\n' % (test['COMPONENT'],test['TESTNAME'], test['TESTSTATUS'] ) 
+		if( test['DEBUG'] ):
+			_debug = test['DEBUG']
+			i=0
+			for var in _debug:
+				# print '%s = %s' % ( var, _debug[i] )
+				_results += " >>Debug: %s\n\n" % (var) 
+			
+		_results += '\n|--------------------------------------------------------------------------------\n'
 	
+	_results += '\n________________________________________________________\n'
+	_results += 'Test results:  Passed=%s, Failed=%s, Errors=%s\n' % (passed,failed,errors)
+	return _results
+
+
+
+
+def parse_line(line):
+	"""
+	From a line of text gets the function name (Only with script. Not tags, yet)
+	"""
+	pattern = re.compile("""
+		(private|package|remote|public)*[ ]*
+		(any|string|array|numeric|boolean|component|struct|void)*[ ]*
+		function[ ]+([_\-a-z][a-z0-9_\-]+)
+		""", 
+		re.VERBOSE|re.MULTILINE|re.IGNORECASE)
+	m = pattern.match(line)
+	#return the 4th gouped regex, which should be the function name
+	ret_val =  m.group(3) if m else ''
+	return ret_val
 
--- tests/plugin_tests.py
+++ tests/plugin_tests.py
@@ -1,30 +1,54 @@
 import sys
 sys.path.append('../')
 sys.path.append('/home/billy/programs/Sublime Text 2/')
+sys.path.append('/home/billy/.config/sublime-text-2/Packages/')
 import re
 import unittest
 import json
 from pprint import pprint
-from mxunit_plugin import mxunit_command
+# from mxunit_plugin import mxunit_command
 
 
 
 class MXUnitTest(unittest.TestCase):
 	
 	def test_read_json_data(self):
-		test_results=open('tests/test_results_fixture.json').read()
-		# pprint (test_results)
+		test_results_raw=open('tests/test_results_fixture.json')
+		tests = json.load( test_results_raw )
+		passes = len( [ test for test in tests if test['TESTSTATUS']=='Passed'] )
+		self.assertEquals( 33, passes )
+		
 	
 	#To Do: Hanlde cffunction name="asdasd" . HTML attribute
 	def test_parse_line_for_test_method_name(self):
 		line = 'function thisIs_thE_test-MEt0D_nam3(asd,asd,asd)'
-		cmd = mxunit_command(None)
-		actual = cmd.parse_line(line)
-		# actual = self.parse_line(line)
-		print actual
-		# return
-		expected = 'thisIs_thE_test-MEt0D_nam3'
-		self.assertEquals( expected,actual )
+		lines= ['	pattern = re.compile("""', 
+				'function thisIs_thE_test-MEt0D_nam3(asd,asd,asd)',
+				'function __filterTestForStruct(){']
+		# cmd = mxunit_command(None)
+		for line in lines:
+			actual = parse_line(line)
+			# actual = self.parse_line(line)
+			print actual
+			# return
+			expected = 'thisIs_thE_test-MEt0D_nam3'
+			# self.assertEquals( expected,actual )
 
+
+
+def parse_line(line):
+	"""
+	From a line of text gets the function name (Only with script. Not tags, yet)
+	"""
+	pattern = re.compile("""
+		(private|package|remote|public)*[ ]*
+		(any|string|array|numeric|boolean|component|struct|void)*[ ]*
+		function[ ]+([_\-a-z][a-z0-9_\-]+)
+		""", 
+		re.VERBOSE|re.MULTILINE|re.IGNORECASE)
+	m = pattern.match(line)
+	#return the 4th gouped regex, which should be the function name
+	ret_val =  m.group(3) if m else ''
+	return ret_val
 
     
