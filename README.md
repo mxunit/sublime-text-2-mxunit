dfd7bbeb106e2086bce40bb16019190780f30fd3
bill shelton <bill@if.io>
2012-05-08 07:18:26 -0400

    init

---
+++ README.md
@@ -0,0 +1,118 @@
+# MXUnit Test Runner Plugin for [Sublime Text 2](http://www.sublimetext.com/)
+
+## Description
+A plugin for Sublime Text 2 that runs MXUnit tests.
+
+
+### News
+
+ - Jan-5-2012: Still unstable, but improving nicely. 
+ - Jan-1-2012: Brand new, unstable, and likely has numerous issues
+
+
+
+## How it works
+
+Open an MXUnit testcase in Sublime Text 2 and then:
+
+__(Using Sublime's Command Pallette)__
+
+ - Type ```shift+ctrl+p``` (Command pallette)
+ - Type ```mx``` to see list of available commands
+ - Select [ ```Run tests``` | ```Run test on line``` | ```Run last test``` | ```Run tests - show failures only```]
+ - (More to come!)
+
+
+![MXUnit Sublime Screenshot](https://github.com/virtix/sublime-text-2-mxunit/raw/master/MXUnit-Sublime-Text-Command_Window.png)
+
+
+__Or (Using keyboard shortcuts)__
+
+ - To run all tests for an MXUnit testcase type ```alt+r```
+ - To run all tests and display only failures type ```ctrl+alt+shift+r```
+ - To run a single test, move your cursor to the test function and type ```shift+alt+r```
+ - To re-run last test type ```ctrl+alt+r```
+
+
+
+__Or (Using mouse clicks)__
+
+  - Right-click on a testcase and select: 
+  - MXUnit > [Run tests | Run test on line | Run last test | Run all tests - show failures only]
+
+
+## Installation
+
+1. Clone this repository to one of the following directories:
+
+ 	- Mac: ~/Library/Application Support/Sublime Text 2/Packages/
+ 	- Windows: %APPDATA%/Sublime Text 2/Packages/
+ 	- Linux: ~/.config/sublime-text-2/Packages/
+
+	E.g,: 
+
+	```bash
+	$ cd ~/.config/sublime-text-2/Packages/
+	$ git clone git://github.com/virtix/sublime-text-2-mxunit.git
+
+	```
+
+2. Edit the ```mxunit.settings``` configuration file located in the plugin's root folder,
+   adding your development-specific settings:
+
+	```bash
+	{
+	
+		"server" : "localhost",
+		"port" : "8301" ,
+		"component_root" : "/" ,
+		"web_root" :  "/home/billy/software/jrun4/servers/dev/cfusion.ear/cfusion.war/" ,
+	
+	}
+	``` 
+	The ```component_root``` is how the web server sees your application. Above assumed all apps are visible from
+	http://localhost:8301/  and all files are stored in the  ```web_root``` directory.
+
+
+## Installation via [Package Control](http://wbond.net/sublime_packages/package_control)
+
+1. Type ```ctrl+shift+p``` (see below)
+2. Select 'Package Control: Install Package' 
+3. Select 'MXUnit'
+4. Follow step #4 above for editing the ```mxunit.settings``` file.
+
+![Package Control Screenshot](https://github.com/virtix/sublime-text-2-mxunit/raw/master/pacakge-control-ss.png)
+
+
+## Docs and Bugs (to do)
+
+ - Docs: https://github.com/virtix/sublime-text-2-mxunit/wiki
+  
+ - Bugs: https://github.com/virtix/sublime-text-2-mxunit/issues
+
+
+## To Do
+Lots - and the project could use some help!  Fork this repository, hack away, and make a pull request.  
+
+These are some ideas/issues:
+
+ - Display a list of test methods in the quick panel and when the method is selected, run the test
+ - Maintain a history of test runs and display that history in a quick panel
+ - Pretty print debug output and wrap it in folding regions
+ - Run all tests in a directory
+ - Implement concurrency for test runs.  This should support long-running test suites
+ - Ant? Or some Python build script?
+ - Key bindings to switch back and forth between test and code
+ - Plugin tests!  Sadly, testing is not baked into Sublime plugin development. Something is needed to make this happen.
+
+
+##References
+
+ - Sublimetext 2 API - http://www.sublimetext.com/docs/2/api_reference.html
+ - How to create a plugin: http://net.tutsplus.com/tutorials/python-tutorials/how-to-create-a-sublime-text-2-plugin/
+ - Unoffical (but very good) docs - http://sublimetext.info/docs/en/
+ - ColdFusion Sublime Tesxt bundle - https://github.com/indynagpal/coldfusion-sublime-text-2
+
+
+
+TM
---
+++ mxunit_plugin.py
@@ -0,0 +1,259 @@
+"""
+This should take the current file (or selected from navigator) and pass it to
+MXUnit, who will return a TestResult (JSON or Text). The plugin should then
+display the results in s Sublime way.
+"""
+ 
+import os
+import json
+import datetime
+import re
+import pprint
+from urllib2 import urlopen,HTTPError
+import sublime_plugin
+import sublime
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class BaseCommand(sublime_plugin.TextCommand):
+	"""
+	Main Command implemented by child commands.
+	"""
+
+	def __init__(self, view):
+		"""
+		Initializing instance members.
+		"""
+		global_settings = sublime.load_settings("mxunit.settings")
+		self.last_run_settings = sublime.load_settings("mxunit-last-run.sublime-settings")
+		self.server = global_settings.get('server', 'localhost')
+		self.view = view
+		self.output_view = None
+		#grab the runner config items
+		self.port = global_settings.get('port', '80')
+		self.web_root = global_settings.get('web_root', '/var/www/html/')
+		self._win = None
+		self.test_items = { 'test-1':{'url':'AAAAAA'},
+							'test-2':{'url':'BBBBBB'},
+							'test-3':{'url':'CCCCCC'},
+							}
+	
+	
+	def show_qp(self):
+		""" Playing with quick panel. Losts of opportunities here! (Run test history, etc.) """
+		panel_items = []
+		self.view.window().show_quick_panel(self.test_items.keys(), self.on_done)	
+	
+
+	def on_done(self,selected_item):
+		""" Playing with quick panel events. Does nothing useful."""
+		keys = self.test_items.keys()
+		key = keys[selected_item]
+		print  self.test_items[ key ]
+	
+
+	def run_test(self, url, edit, show_failures_only=False):
+		"""
+		Main test runner. Intended to be called from child command. 
+		"""
+		
+		try:
+			_res = urlopen(url)
+			self._win = self.view.window()
+			self._results = _res.read()
+			self.view.window().run_command("show_panel", {"panel": "output.tests"})
+			self.output_view = self.view.window().get_output_panel("tests")
+			self.output_view.insert( edit, self.output_view.size(), pretty_results(self._results, show_failures_only) ) 
+			self.save_test_run(url,show_failures_only)
+
+		except HTTPError as e:
+			sublime.error_message ('\nRuh roh, Raggy. Are you sure this is a valid MXUnit test?\n\n%s' % e)
+
+	
+	
+	def save_test_run(self, url, show_failures_only):
+		"""
+		Persists the last run test. 
+
+		To Do:  Save it as a stack with a MAX num. Stack can be displayed with the quick panel.
+		"""
+		print 'Saving url: %s' % url
+		self.last_run_settings.set("last_test_run", url)
+		self.last_run_settings.set("show_failures_only", show_failures_only)
+		sublime.save_settings("mxunit-last-run.sublime-settings")
+		print self.last_run_settings.get('last_test_run')
+
+
+
+class ShowQpCommand(BaseCommand):
+	""" 
+	Just playing with the quick panel. Does nothing except confuse the user. 
+
+	To Do: 
+		(1)  Could show a history of test runs and run the selected history item.
+		(2)  Could display a list of valid tests in current file and run the selected one.
+ 	"""
+	def run(self,edit):
+		self.show_qp()	
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class HideTestPanelCommand(BaseCommand):
+	""" 
+	Hide the test results panel (esc works fine, but whatever...)
+ 	"""
+	def run(self,edit):
+		self.view.window().run_command("hide_panel", {"panel": "output.tests"})	
+
+
+class ShowTestPanelCommand(BaseCommand):
+	""" 
+	Shows the test results panel
+ 	"""
+	def run(self,edit):
+		self.view.window().run_command("show_panel", {"panel": "output.tests"})			
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class MxunitCommand(BaseCommand):
+	"""
+	Runs all tests in an MXUnit testcase.
+	"""
+	def run(self,edit):
+		_view = self.view
+		_current_file = _view.file_name()
+		#test
+		_test_cfc = _current_file.replace(self.web_root, '')
+		print 'Test: %s' % _test_cfc
+		_url = 'http://' + self.server + ':' + self.port + '/' + _test_cfc +'?method=runtestremote&output=json'
+		self.run_test(_url, edit)
+		
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class RunAllFailuresOnlyCommand(BaseCommand):
+	"""
+	Runs all tests in an MXUnit testcase but display only failures.
+	"""
+	def run(self,edit):
+		_view = self.view
+		_current_file = _view.file_name()
+		#test
+		_test_cfc = _current_file.replace(self.web_root, '')
+		print 'Test: %s' % _test_cfc
+		_url = 'http://' + self.server + ':' + self.port + '/' + _test_cfc +'?method=runtestremote&output=json'
+		self.run_test(_url, edit,show_failures_only=True)
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class RunLastTestCommand(BaseCommand):
+	"""
+	Looks up the last run test and simly runs it.
+	"""
+	def run(self,edit):
+		_url = self.last_run_settings.get("last_test_run")
+		_show_failures = self.last_run_settings.get("show_failures_only")
+		self.run_test(_url, edit,_show_failures)
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class SingleTestCommand(BaseCommand):
+	"""
+	Runs a single MXUnit test.  Expect that user has placed cursor on line where test
+	exists.  We parse the line, extracting the test name and pass that to MXUnit.
+	"""
+	def run(self,edit):
+		_view = self.view
+		for region in _view.sel():
+			line = _view.line(region)
+			line_contents = _view.substr(line) + '\n'
+			test_method = parse_line(line_contents)
+		
+		_current_file = _view.file_name()
+		#test
+		_test_cfc = _current_file.replace(self.web_root, '')
+		print 'Test: %s - %s' %  (_test_cfc, test_method,)
+		if test_method == '' : 
+			sublime.error_message ('\nRuh roh, Raggy. The line the cursor is on doesn\'t look like a test.\n\n')
+			return
+		_url = 'http://' + self.server + ':' + self.port + '/' + _test_cfc +'?method=runtestremote&output=json&testmethod=' + test_method
+		self.run_test(_url, edit)
+
+
+
+
+
+
+#########################################################################################
+#                                                                                       #
+#           Utility methods used by all classes (could be in BaseCommand)               #
+#                                                                                       #  
+#########################################################################################
+
+def pretty_results(test_results, show_failures_only):
+	"""
+	Format JSON to string output.  
+
+	(To Do: use Python template and JSON as context)
+	"""
+
+	_results =  '__________________________________________________________________________________\n\n'
+	_results += '		:::::::   MXUnit Test Results  :::::::     \n'
+	tests = json.loads(test_results)
+	 
+	passed =  len( [ x for x in tests if x['TESTSTATUS']=='Passed'] )
+	failed =  len( [ x for x in tests if x['TESTSTATUS']=='Failed'] )
+	errors =  len( [ x for x in tests if x['TESTSTATUS']=='Error'] )
+	total_time  =  len( [ x for x in tests if x['TIME'] > 0] )
+	_results += '		Passed: %s, Failed: %s, Errors: %s, Time: %sms\n' % (passed,failed,errors,total_time)
+	_results += '		Date:  %s\n' % (datetime.datetime.now().strftime("%A, %B %d, %Y, %I:%M %p"))
+	_results += '__________________________________________________________________________________\n\n'
+
+	if show_failures_only:
+		_results += '				  *** Showing Failures Only ***  \n\n'
+		tests = [ _test for _test in tests if _test['TESTSTATUS']=='Failed' ]
+
+	for test in tests:
+		_results += '	%s.%s (%s) %sms\n' % (test['COMPONENT'], test['TESTNAME'], test['TESTSTATUS'], test['TIME'] ) 
+		
+		if( test['TESTSTATUS'] in ('Failed','Error') ):
+			_results += '		Message: %s\n' % (test['ERROR']['Message'])
+			_results += '		StackTrace: %s\n' % (test['ERROR']['StackTrace'])
+
+		if( test['DEBUG'] ):
+			_debug = test['DEBUG']
+			i=0
+			for var in _debug:
+				# print '%s = %s' % ( var, _debug[i] )
+				_results += "		Debug: 	%s \n " % ( var['VAR'] ) 
+			
+		_results += '\n|--------------------------------------------------------------------------------\n'
+	
+	_results += '\n__________________________________________________________________________________\n\n'
+	_results += 'Test results:  Passed=%s, Failed=%s, Errors=%s\n' % (passed,failed,errors)
+	return _results
+
+
+
+
+
+def parse_line(line):
+	"""
+	From a line of text gets the function name.
+	"""
+	pattern = re.compile("""
+		[ \t]*
+		(private|package|remote|public)*[ ]*
+		(any|string|array|numeric|boolean|component|struct|void)*[ ]*
+		(function[ ]+|\<cffunction[ ]+name[ ]*=[ ]*\"?)([_\-a-z][a-z0-9_\-]+)
+		""", 
+		re.VERBOSE|re.MULTILINE|re.IGNORECASE)
+	m = pattern.match(line)
+	#return the 5th gouped regex, which should be the function name
+	ret_val =  m.group(4) if m else ''
+	return ret_val
+
