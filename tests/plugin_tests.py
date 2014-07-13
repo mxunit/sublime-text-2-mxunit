f7024f93803723a8980cff99ee195ac757c35e67
ckaznocha <ckaznocha@gmail.com>
2014-07-13 13:57:31 -0400

    Fix test_results_fixture.json pat

--- tests/plugin_tests.py
+++ tests/plugin_tests.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.append('../')
 sys.path.append('/home/billy/programs/Sublime Text 2/')
 sys.path.append('/home/billy/.config/sublime-text-2/Packages/')
@@ -15,7 +16,8 @@ from pprint import pprint
 class MXUnitTest(unittest.TestCase):
 	
 	def test_read_json_data(self):
-		test_results_raw=open('test_results_fixture.json')
+		fn = os.path.join(os.path.dirname(__file__), 'test_results_fixture.json')
+		test_results_raw=open(fn)
 		tests = json.load( test_results_raw )
 		# pprint(tests)
 		passes = len( [ test for test in tests if test['TESTSTATUS']=='Passed'] )
